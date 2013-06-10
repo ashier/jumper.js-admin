232b9750b807182a564c741646cb63f9570dff68
Ashier de Leon <ashier@gmail.com>
2013-06-10 23:44:50 +0800

    Initial Commit

---
+++ gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+
+  // Default task(s).
+  grunt.registerTask('default', []);
+
+};
